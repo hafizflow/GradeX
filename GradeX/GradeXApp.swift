aedb670f319a77cacfbd527ed5fb3391c6eae79d
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-10-24 05:56:12 +0600

    Initial Commit

---
+++ GradeX/GradeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradeXApp.swift
+//  GradeX
+//
+//  Created by Hafizur Rahman on 24/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GradeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
